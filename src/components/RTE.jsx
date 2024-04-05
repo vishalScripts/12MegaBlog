cb6bf2b1da270f9c320f3168056f54bcb99b5f32
myself-viishal <3953vishal@gmail.com>
2024-04-05 13:51:59 +0530

    edited the setting of RTE

--- src/components/RTE.jsx
+++ src/components/RTE.jsx
@@ -12,7 +12,7 @@ export default function RTE({ name, control, label, defaultValue = "" }) {
         control={control}
         render={({ field: { onChange } }) => (
           <Editor
-            apiKey={conf.tinymceApiKey}
+            apiKey={String(import.meta.env.VITE_TINY_MCE_API_KEY)}
             initialValue={defaultValue}
             init={{
               initialValue: defaultValue,
