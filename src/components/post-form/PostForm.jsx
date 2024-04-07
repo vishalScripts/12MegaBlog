d331c57ca31873484a1b199e4d6e3241eb1f92d9
myself-viishal <3953vishal@gmail.com>
2024-04-07 12:59:38 +0530

    Fixed - update post [url not changing]

--- src/components/post-form/PostForm.jsx
+++ src/components/post-form/PostForm.jsx
@@ -39,25 +39,43 @@ export default function PostForm({ post }) {
         appwriteService.deleteFile(post.featuredImage);
       }
 
-      const dbPost = await appwriteService.updatePost(post.$id, {
+      // data.$id = data.slug
+
+      // console.log(data)
+      // const dbPost = await appwriteService.updatePost(post.$id, {
+      //   ...data,
+      //   featuredImage: file ? file.$id : undefined,
+      // });
+
+      console.log(file);
+      const fileId = file ? file.$id : post.featuredImage;
+      data.featuredImage = fileId;
+      const dbPost = await appwriteService.createPost({
         ...data,
-        featuredImage: file ? file.$id : undefined,
+        title: capitalizeFirstLetter(data.title),
+        userId: userData.$id,
       });
 
       if (dbPost) {
-        appwriteService
-          .getPosts([])
-          .then((posts) => {
-            if (posts) {
-              dispatch(addPosts(posts.documents));
-              useUploadPosts(dispatch);
-              return true;
-            }
-          })
-          .catch((error) => {
-            console.error("Error fetching posts:", error);
-            return false;
-          });
+        const delPost = await appwriteService.deletePost(post.$id);
+        console.log({ ...data });
+        console.log("slug----------------", data.slug);
+        console.log("db post-----------------", dbPost.$id);
+        if (delPost) {
+          appwriteService
+            .getPosts([])
+            .then((posts) => {
+              if (posts) {
+                // dispatch(addPosts(posts.documents));
+                useUploadPosts(dispatch);
+                return true;
+              }
+            })
+            .catch((error) => {
+              console.error("Error fetching posts:", error);
+              return false;
+            });
+        } else console.log("error is deliting the post while updating");
         navigate(`/post/${dbPost.$id}`);
       }
     } else {
